093da438fc5e06b86d6daaf3b763828d242651a3
emin <samburer200@gmail.com>
2020-05-10 15:20:01 +0300

    add validation for signIn

--- src/main/java/ru/itis/biology/api/SignInRestController.java
+++ src/main/java/ru/itis/biology/api/SignInRestController.java
@@ -1,11 +1,18 @@
 package ru.itis.biology.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import ru.itis.biology.dto.SignInDto;
 import ru.itis.biology.dto.TokenDto;
 import ru.itis.biology.service.SignInService;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/signIn")
@@ -15,8 +22,22 @@ public class SignInRestController {
     private SignInService signInService;
 
     @PostMapping
-    public ResponseEntity<TokenDto> signIn(@RequestBody SignInDto signInDto) {
+    public ResponseEntity<TokenDto> signIn(@Valid @RequestBody SignInDto signInDto) {
         return ResponseEntity.ok(signInService.signIn(signInDto));
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
 
 }
--- src/main/java/ru/itis/biology/dto/SignInDto.java
+++ src/main/java/ru/itis/biology/dto/SignInDto.java
@@ -2,8 +2,19 @@ package ru.itis.biology.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 public class SignInDto {
+
+    @NotNull
+    @Email
     private String email;
+
+    @NotNull
+    @Size(min = 5, max = 22)
     private String password;
 }
